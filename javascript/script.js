96d06b1d29029fc62d3cd93fd9977252aca4eebe
Kenab <kebab.bahadur@gmail.com>
2024-04-28 11:02:52 +0545

    Fixed Responsive Bugs in the nav bar

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,18 @@ const header = document.querySelector(".header");
 
 navBtn.addEventListener("click", function () {
   header.classList.toggle("nav-btn-open");
+  changeScroll();
 });
+
+const changeScroll = () => {
+  if (header.classList.contains("nav-btn-open")) {
+    document.documentElement.style.overflow = "hidden";
+    document.addEventListener("keydown", function (event) {
+      if (event.keyCode == 27) {
+        header.classList.remove("nav-btn-open");
+      }
+    });
+  } else {
+    document.documentElement.style.overflow = "scroll";
+  }
+};
